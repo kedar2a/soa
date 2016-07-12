5852c09ec063a2bb5a2cf5d5de321f6d1b4e0bb7
Kedar Aitawdekar <kedar2a@gmail.com>
2016-07-13 00:42:10 +0530

    Included vegas slideshow

--- idols/views.py
+++ idols/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('idols/base.html')
+    template = loader.get_template('idols/landing-slider-page.html')
     context = {
         'test': 'test',
     }
