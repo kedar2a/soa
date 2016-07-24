c7a42be0bebce704ef43c577d459e6e38cd9659b
Kedar Aitawdekar <kedar2a@gmail.com>
2016-07-25 01:14:22 +0530

    Added all urls and views for site

--- idols/urls.py
+++ idols/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home_slider, name='home_slider'),
+    url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^aim$', views.aim, name='aim'),
+    url(r'^media$', views.media, name='media'),
+    url(r'^team$', views.team, name='team'),
+    url(r'^contact$', views.contact, name='contact'),
 ]
--- idols/views.py
+++ idols/views.py
@@ -2,8 +2,48 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
-    template = loader.get_template('idols/landing-slider-page.html')
+def home_slider(request):
+    template = loader.get_template('idols/home-slider.html')
+    context = {
+        'test': 'test',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def gallery(request):
+    template = loader.get_template('idols/gallery.html')
+    context = {
+        'test': 'test',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def aim(request):
+    template = loader.get_template('idols/aim.html')
+    context = {
+        'test': 'test',
+    }
+    return HttpResponse(template.render(context, request))    
+
+
+def media(request):
+    template = loader.get_template('idols/media.html')
+    context = {
+        'test': 'test',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def team(request):
+    template = loader.get_template('idols/team.html')
+    context = {
+        'test': 'test',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def contact(request):
+    template = loader.get_template('idols/contact.html')
     context = {
         'test': 'test',
     }
