56b3b00f894df1a261cfdb50871759d7bed06ceb
Kedar Aitawdekar <kedar2a@gmail.com>
2016-07-25 01:19:07 +0530

    Added some files in .gitignore

--- SOA_Project/settings.py
+++ SOA_Project/settings.py
@@ -149,16 +149,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-# try:
-#     # AWS keys
-#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
-#     AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
+try:
+    # AWS keys
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
 
-#     DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
-#     MEDIA_URL = 'http://%s.s3.amazonaws.com/idol-images/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = 'http://%s.s3.amazonaws.com/idol-images/' % AWS_STORAGE_BUCKET_NAME
 
-#     from local_settings import *
-# except ImportError:
-#     pass
+    from local_settings import *
+except ImportError:
+    pass
