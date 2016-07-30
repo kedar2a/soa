53a13c15402e006ce50b777d1aab1590f8483bfe
Kedar Aitawdekar <kedar2a@gmail.com>
2016-07-30 20:37:51 +0530

    added complete url traversal till murti

--- idols/urls.py
+++ idols/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_slider, name='home_slider'),
     url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^gallery/(?P<murti_category_code>[\w-]+)$', views.murti_category, name='murti_category'),
+    url(r'^gallery/(?P<murti_category_code>[\w-]+)/(?P<m_id>[\w-]+)$', views.murti_detail, name='murti_detail'),
     url(r'^aim$', views.aim, name='aim'),
     url(r'^media$', views.media, name='media'),
     url(r'^team$', views.team, name='team'),
--- idols/views.py
+++ idols/views.py
@@ -11,9 +11,42 @@ def home_slider(request):
 
 
 def gallery(request):
+
+    from idols.models import MurtiCategory
+
+    all_murti_category = MurtiCategory.objects.all()
     template = loader.get_template('idols/gallery.html')
     context = {
-        'test': 'test',
+        'all_murti_category': all_murti_category,
+        'url_name': 'murti_category'
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def murti_category(request, murti_category_code):
+
+    from idols.models import MurtiCategory, Murti
+
+    template = loader.get_template('idols/gallery.html')
+    murti_category_obj = MurtiCategory.objects.get(category_code=murti_category_code)
+    all_category_murtis = Murti.objects.filter(category_id=murti_category_obj.id)
+
+    context = {
+        'all_murti_category': all_category_murtis,
+        'url_name': 'murti_detail'
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def murti_detail(request, murti_category_code, m_id):
+
+    from idols.models import Murti
+
+    template = loader.get_template('idols/murti-detail.html')
+    murti_obj = Murti.objects.get(id=m_id)
+
+    context = {
+        'murti': murti_obj,
     }
     return HttpResponse(template.render(context, request))
 
@@ -48,3 +81,4 @@ def contact(request):
         'test': 'test',
     }
     return HttpResponse(template.render(context, request))
+
